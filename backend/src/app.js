f751db8f063dccec42c04f32a7e32969d2f489b2
jodsab <camilo.b.q@hotmail.com>
2023-06-10 14:30:22 -0500

    feat camilo

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,29 +1,28 @@
 import express from "express";
 import morgan from "morgan";
-//import cors from "cors";
+import cors from "cors";
 
-import objetivos_router from "../src/routes/objetivos_router.js"
-import metrica_router from "../src/routes/metrica_router.js"
-import progreso_router from "../src/routes/progreso_router.js"
+import objetivos_router from "../src/routes/objetivos_router.js";
+import metrica_router from "../src/routes/metrica_router.js";
+import progreso_router from "../src/routes/progreso_router.js";
 
 const app = express();
 
-//Settings 
+//Settings
 app.set("port", 4000);
 
-
-//Middlewares 
-//app.use(cors({
-//    origin: '*'
-//}));
+//Middlewares
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 
-
-
-//Routes 
+//Routes
 app.use("/api/objetivos", objetivos_router);
 app.use("/api/metrica", metrica_router);
 app.use("/api/progreso", progreso_router);
 
-export default app; 
+export default app;
--- backend/src/controllers/objetivos_controller.js
+++ backend/src/controllers/objetivos_controller.js
@@ -1,53 +1,70 @@
-import {getConnection} from "./../database/database.js";
+import { getConnection } from "./../database/database.js";
 
 import mysql from "mysql2/promise.js";
 import config from "../config.js";
 
-
-const getObjetivos = async(req, res) =>{
-    try {
-        const connection = await getConnection(); 
-        const result = await connection.promise().query("SELECT  * FROM objetivo"); 
-        console.log(result);
-        res.json(result); 
-    } catch (error) {
-        res.status(500); 
-        res.send(error.message); 
-    }
-    
+const getObjetivos = async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const result = await connection.promise().query("SELECT  * FROM objetivo");
+    res.json(result);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
 };
 
-const getObjetivo = async(req, res) =>{
-    try {
-        console.log(req.params);
-        const {id} = req.params;  
-        const idobjetivos = id;
-        const connection = await getConnection(); 
-        const result = await connection.promise().query("SELECT * FROM objetivo WHERE idobjetivos =?", idobjetivos); 
-        res.json({message: result}); 
-    } catch (error) {
-        res.status(500); 
-        res.send(error.message); 
-    }
-    
+const getObjetivo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const idobjetivos = id;
+    const connection = await getConnection();
+    const result = await connection
+      .promise()
+      .query("SELECT * FROM objetivo WHERE idobjetivos =?", idobjetivos);
+    res.json({ message: result });
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
 };
 
-const addobjetivos = async (req, res) =>{
-    try {
-        const {idobjetivo, nombre, descripcion, porcentaje, persona, idmetrica, fechainicio, fecahfin, meta, aceptable} = req.body;
-        const objetivo = {idobjetivo, nombre, descripcion, porcentaje, persona, idmetrica, fechainicio, fecahfin, meta, aceptable }; 
-        const connection = await getConnection();
-        await connection.promise().query("INSERT INTO objetivos SET ?", objetivo);
-        res.json({message: "Objetivo Added"}); 
-      
-    } catch (error) {
-        res.status(500); 
-        res.send(error.message); 
-    }
+const addobjetivos = async (req, res) => {
+  try {
+    const {
+      nombre,
+      descripcion,
+      porcentaje,
+      persona,
+      id_metrica,
+      fechainicio,
+      fechafin,
+      meta,
+      aceptable,
+    } = req.body;
+    const objetivo = {
+      nombre,
+      descripcion,
+      porcentaje,
+      persona,
+      id_metrica,
+      fechainicio,
+      fechafin,
+      meta,
+      aceptable,
+    };
+    const connection = await getConnection();
+    console.log("objetivo", objetivo);
+    await connection.promise().query("INSERT INTO objetivos SET ?", objetivo);
+    res.json({ message: "Objetivo Added" });
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
 };
 
 export const methods = {
-    getObjetivos,
-    addobjetivos,
-    getObjetivo
-}; 
+  getObjetivos,
+  addobjetivos,
+  getObjetivo,
+};
